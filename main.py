8dc23b39776ed1b426ebc580a34317639366062f
Vancha <tjipkevanderheide90@gmail.com>
2022-08-10 15:50:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ clock = pygame.time.Clock()
 running = True
 chess_board = chess_board(WINDOW_WIDTH, WINDOW_HEIGHT)
 
+dragging = False
+selected = False
 while running == True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -23,9 +25,31 @@ while running == True:
             if event.key == K_ESCAPE:
                 pygame.quit()
                 running = not running
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            position = pygame.mouse.get_pos()
+            row = chess_board.location_to_board_index(position[0])
+            col = chess_board.location_to_board_index(position[1])
+            selected = chess_board.get_piece_at_location((row,col))
+
+            if not selected == False:
+                selected.invisible = True
+                chess_board.board[row][col] = False
+                dragging = True
+        elif event.type == pygame.MOUSEBUTTONUP:
+            position = pygame.mouse.get_pos()
+            row = chess_board.location_to_board_index(position[0])
+            col = chess_board.location_to_board_index(position[1])
+            selected.position = (row,col)
+            chess_board.board[row][col] = selected
+            selected.invisible = False
+            dragging = False
+            selected = False
     if not running:
         break;
 
     chess_board.draw(display)
+    if dragging:
+        loc = pygame.mouse.get_pos()
+        display.blit(selected.image, loc)
     clock.tick(60)
-    pygame.display.flip()
+    pygame.display.update()
