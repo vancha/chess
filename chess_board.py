68a9b0d4846edb08bf020445dc36e493268d78bc
Vancha <tjipkevanderheide90@gmail.com>
2022-08-10 10:51:23 +0200

    Update chess_board.py

--- chess_board.py
+++ chess_board.py
@@ -1,1 +1,13 @@
 
+class chess_board():
+    def __init__(self):
+        self.board = [[False for x in range(21)] for y in range(21)]
+
+    def draw(self, screen):
+        print(self.board)
+
+    def place_piece(self, piece):
+        pass
+
+    def initialize_all_pieces(self):
+        pass
